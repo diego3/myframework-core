0fd50417002ea17914b6531335ea6e2d6c6acf62
Diego rosa dos santos <www.diegosantos.com.br@gmail.com>
2015-05-14 10:18:48 -0300

    - fix receber paramentros do tipo array : ProccessRequest

--- src/Request/ProcessRequest.php
+++ src/Request/ProcessRequest.php
@@ -335,6 +335,10 @@ abstract class ProcessRequest {
                     continue;
                 }
                 $received = filter_input(RequestType::getInternalInput($method), $itemname, FILTER_UNSAFE_RAW);
+                if(!$received) {
+                    # se é false pode ser array ?
+                    $received = filter_input_array(RequestType::getInternalInput($method), array($itemname => array('flags' => FILTER_REQUIRE_ARRAY)));
+                }
                 $log->debug('$_' . $method . '["' . $itemname . '"]: ' . var_export($received, true));                
                 $type = Factory::datatype($itemdata['type']);
                 $cleaned = $type->sanitize($received, $itemdata['params']);//faz os filtros, verifica os tipos e tals
